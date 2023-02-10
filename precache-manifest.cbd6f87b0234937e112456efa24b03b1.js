400cbc0443e308492eb15024ea515df459c73743
Darshan Zunjurde <darshanzunjurde@gmail.com>
2023-02-10 16:33:58 +0530

    Updates

---
+++ precache-manifest.cbd6f87b0234937e112456efa24b03b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2c70f9d9803ebc373c9",
+    "url": "/Home/static/css/main.58247457.chunk.css"
+  },
+  {
+    "revision": "f2c70f9d9803ebc373c9",
+    "url": "/Home/static/js/main.f2c70f9d.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "/Home/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "9fcf9f1481a2b4258b36",
+    "url": "/Home/static/css/2.d29e8108.chunk.css"
+  },
+  {
+    "revision": "9fcf9f1481a2b4258b36",
+    "url": "/Home/static/js/2.9fcf9f14.chunk.js"
+  },
+  {
+    "revision": "221d2ac426e939d9b160",
+    "url": "/Home/static/js/runtime~main.221d2ac4.js"
+  },
+  {
+    "revision": "89819c33adc47b2efc37c701a59b4f65",
+    "url": "/Home/index.html"
+  }
+];
